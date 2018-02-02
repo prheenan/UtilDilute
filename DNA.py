319e75974b988facfb97fc1de84735f56e8e9c56
Your Name <patirck.heenan@colorado.edu>
2018-02-02 14:06:25 -0700

    defaults to dsDNA now...

--- DNA.py
+++ DNA.py
@@ -11,17 +11,20 @@ import sys
 
 from Bio.SeqUtils import molecular_weight
 
-def mw_DNA(seq=None,**kw):
+def mw_DNA(seq=None,seq_type='DNA',double_stranded=True,**kw):
     """
     :param seq: DNA sequence; otherwise just gets one average bp mass
     :return: Molecular weight, in daltons
     """
+    opt = dict(seq_type=seq_type,
+               double_stranded=double_stranded,
+               **kw)
     if (seq is None):
-        mass = [molecular_weight(seq=str(s), seq_type='DNA') 
+        mass = [molecular_weight(seq=str(s), **opt)
                 for s in ["A","T","G","C"]]
         return np.mean(mass)
     # POST: some sequence to use
-    return Bio.molecular_weight(seq=s, seq_type='DNA')
+    return Bio.molecular_weight(seq=s, **opt)
 
 def dalton_to_g():
     return 1.66e-23
