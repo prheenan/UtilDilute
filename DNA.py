a8efbcd219ab7e066b5e5756986afb946a916f18
Patrick Laptop (MBP) <patirck.heenan@colorado.edu>
2018-05-18 14:32:30 -0600

    added in unit testing for DNA conversions...

--- DNA.py
+++ DNA.py
@@ -24,8 +24,44 @@ def mw_DNA(seq=None,seq_type='DNA',double_stranded=True,**kw):
                 for s in ["A","T","G","C"]]
         return np.mean(mass)
     # POST: some sequence to use
-    return Bio.molecular_weight(seq=s, **opt)
+    return molecular_weight(seq=seq, **opt)
+
+def nM_per_ng_per_uL(seq=None,seq_len=None):
+    if seq is None:
+        assert seq_len is not None
+        molar_mass = mw_DNA() * seq_len
+    else:
+        molar_mass = mw_DNA(seq=seq)
+    # get the conversion from ng/uL to g/L
+    to_g_per_L = 1e-3
+    to_mole_per_L = to_g_per_L/molar_mass
+    to_n_mol_per_L = to_mole_per_L * 1e9
+    return to_n_mol_per_L
+    
 
 def dalton_to_g():
     return 1.66e-23
 
+def _check_nM():
+    """
+    Make sure the conversions are OK within a pct or so. 
+
+    see: support.illumina.com/bulletins/2016/11/converting-ngl-to-nm-when-calculating-dsdna-library-concentration-.html
+    
+    They just say:
+
+    [nM] = ([ng/uL] / ( (660 g/mol) * N_bp)) * 1e6
+    """
+    lens = [1,10,1000]
+    f_expected = lambda _x: 1/(660 * _x) * 1e6
+    for l in lens:
+        expected = f_expected(l)
+        actual = nM_per_ng_per_uL(seq_len=l)
+        np.testing.assert_allclose(expected,actual,atol=0,rtol=1e-2)
+        
+def test():
+    _check_nM()
+
+
+if __name__ == "__main__":
+    test()
